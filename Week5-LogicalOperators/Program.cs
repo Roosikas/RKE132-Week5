67d5104b10d53cd13e5db75fecd433015d49d3e1
Roosikas <jan.roos@tktk.ee>
2024-12-19 22:59:06 +0200

    add week5 code

---
+++ Week5-LogicalOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+AND - &&
+Output=true if both inputs=true
+
+true && true --> true 
+true && false --> false 
+false && true --> false 
+false && false --> false
+
+----
+
+OR - || (pipes)
+Output=true if any input=true
+
+true || true --> true
+true || false --> true //false || true --> true
+false || false --> false
+*/
